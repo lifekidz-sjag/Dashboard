779b506ac920f66977cef6a94a514374dabeea1d
Oon Chee Yee <ooncheeyee@ximnetmy.onmicrosoft.com>
2024-12-06 00:45:19 +0800

    test

--- src/features/students/useAddStudent.jsx
+++ src/features/students/useAddStudent.jsx
@@ -9,16 +9,13 @@ import {
   IconButton,
   Stack,
   Typography,
-  useMediaQuery,
 } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 import CryptoJS from "crypto-js";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import QRCode from "qrcode.react";
 import * as yup from "yup";
 
-import formBg from "../../assets/form-bg.png";
 import {
   FormDateTimePicker,
   FormSelect,
@@ -38,8 +35,6 @@ const useAddStudent = ({
   sharedState,
   sharedFunction,
 }) => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
   const [qrCodeData, setQRCodeData] = useState("");
 
   // API service
@@ -145,7 +140,8 @@ const useAddStudent = ({
           onSubmit={handleSubmitCreate(handleAdd)}
           sx={{
             overflow: "auto",
-            maxHeight: { xs: "calc(80vh - 140px)", md: "100vh" },
+            maxHeight: { xs: "80vh", md: "100vh" },
+            paddingBottom: "80px",
           }}
         >
           <Box
@@ -185,13 +181,6 @@ const useAddStudent = ({
             <Box
               sx={{
                 flex: "1 1 auto",
-                minHeight: {
-                  xs: "calc(100vh - 400px)",
-                },
-                height: {
-                  xs: "calc(100vh - 400px)",
-                },
-                padding: "0px 0px 160px",
                 boxSizing: "border-box",
                 position: "relative",
                 zIndex: 1,
@@ -343,52 +332,35 @@ const useAddStudent = ({
               </Grid>
             </Box>
           </Box>
-
           <Box
             sx={{
-              position: "absolute",
-              width: "100%",
-              bottom: { xs: "75px", md: "0px" },
-              right: "0px",
-              background: "white",
-              zIndex: 1,
-              borderTop: "1px solid rgba(0,0,0,0.1)",
+              padding: "16px 24px 24px 24px",
+              display: "flex",
+              justifyContent: "flex-end",
             }}
           >
-            {!isSmallScreen && (
-              <Box component="img" src={formBg} sx={{ width: "100%" }} />
-            )}
-
-            <Box
-              sx={{
-                padding: "16px 24px 24px 24px",
-                display: "flex",
-                justifyContent: "flex-end",
-              }}
-            >
-              <Stack spacing={5} direction="row">
-                <Button
-                  variant="text"
-                  size="large"
-                  style={{
-                    borderRadius: "100px",
-                  }}
-                  onClick={sidebar.close}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                  style={{
-                    borderRadius: "100px",
-                  }}
-                >
-                  Create
-                </Button>
-              </Stack>
-            </Box>
+            <Stack spacing={5} direction="row">
+              <Button
+                variant="text"
+                size="large"
+                style={{
+                  borderRadius: "100px",
+                }}
+                onClick={sidebar.close}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                size="large"
+                type="submit"
+                style={{
+                  borderRadius: "100px",
+                }}
+              >
+                Create
+              </Button>
+            </Stack>
           </Box>
         </Box>
       </Box>
